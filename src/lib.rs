7acaa01770c21f0302b676f613f47f9a43ea006d
Luciano <lucianomammino@gmail.com>
2021-04-10 20:05:45 +0100

    Added docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,56 @@
+//! `rollz` is a super-simple Rust crate that allows you to... roll dice! What else would you need from a crate?!
+//!
+//! This crate implements standard D&D dice set: `D4`, `D6`, `D8`, `D10`, `D12` and `D20`.
+//!
+//! Here's an example:
+//!
+//! ```rust
+//! use rollz::prelude::*;
+//! use rollz::D10;
+//!
+//! let d: D10 = roll();
+//! assert!((1..=10).contains(&d.val()));
+//! ```
+//!
+//! You can also implement your own custom die if you implement the `Rollable` trait:
+//!
+//! ```rust
+//! use rollz::prelude::*;
+//!
+//! struct Fake100(u8);
+//!
+//! impl Rollable for Fake100 {
+//!     fn roll() -> Fake100 {
+//!         Fake100 { 0: 100 }
+//!     }
+//!     fn val(&self) -> u8 {
+//!         self.0
+//!     }
+//! }
+//!
+//! let d: Fake100 = roll();
+//! assert_eq!(100, d.val());
+//!```
+
 use rand::{thread_rng, Rng};
 
+/// This is the trait that every die needs to implement to be... well... "rollable", right?
 pub trait Rollable {
+    /// Roll the die
     fn roll() -> Self;
+    /// Get the value from the latest roll
     fn val(&self) -> u8;
 }
 
+/// A generic function to roll a given die.
+/// It uses return type polymorphism so it will roll the die you actually want! Boom, like magic!
 pub fn roll<T: Rollable>() -> T {
     Rollable::roll()
 }
 
 pub mod prelude;
 
+/// A D4 die (4 faces): a roll will give you a `u8` in the `1..=4` range.
 #[derive(Debug)]
 pub struct D4(u8);
 impl Rollable for D4 {
@@ -24,6 +64,7 @@ impl Rollable for D4 {
     }
 }
 
+/// A D6 die (6 faces): a roll will give you a `u8` in the `1..=6` range.
 #[derive(Debug)]
 pub struct D6(u8);
 impl Rollable for D6 {
@@ -37,6 +78,7 @@ impl Rollable for D6 {
     }
 }
 
+/// A D8 die (8 faces): a roll will give you a `u8` in the `1..=8` range.
 #[derive(Debug)]
 pub struct D8(u8);
 impl Rollable for D8 {
@@ -50,6 +92,7 @@ impl Rollable for D8 {
     }
 }
 
+/// A D10 die (10 faces): a roll will give you a `u8` in the `1..=10` range.
 #[derive(Debug)]
 pub struct D10(u8);
 impl Rollable for D10 {
@@ -63,6 +106,7 @@ impl Rollable for D10 {
     }
 }
 
+/// A D12 die (12 faces): a roll will give you a `u8` in the `1..=12` range.
 #[derive(Debug)]
 pub struct D12(u8);
 impl Rollable for D12 {
@@ -76,6 +120,7 @@ impl Rollable for D12 {
     }
 }
 
+/// A D20 die (20 faces): a roll will give you a `u8` in the `1..=20` range.
 #[derive(Debug)]
 pub struct D20(u8);
 impl Rollable for D20 {
